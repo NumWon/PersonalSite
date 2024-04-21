bf7df0c9431fc421c64b56f58f10b45ec87a2d02
Naumaan <naumaansheikh30@gmail.com>
2024-04-21 17:11:41 -0400

    slight changes to header, finished the Intro section

--- src/Sections/AboutMe.js
+++ src/Sections/AboutMe.js
@@ -1,9 +1,10 @@
 import React from "react";
+import './AboutMe.css';
 
 function AboutMe() {
 	return (
-		<section id='about'>
-			<div>
+		<section id='about' className="about-section">
+			<div className="about-container">
 				<h2>About Me</h2>
 				<img />
 				<p className='about-paragraph'>
--- src/Sections/Intro.js
+++ src/Sections/Intro.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import './Intro.css';
+import ScrollIcon from '../Assets/down-icon.svg';
 
 function Intro() {
+	const handleScroll = () => {
+		const nextSection = document.getElementById('about');
+		if (nextSection) {
+			nextSection.scrollIntoView({behavior: 'smooth'});
+		}
+	};
+
 	return (
-		<section id='intro'>
-			<div>
+		<section id='intro' className='intro-section'>
+			<div className='intro-container'>
 				<h2>Hey, I'm Naumaan Sheikh...</h2>
 				<p className='intro-paragraph'>I'm an aspiring Software Engineer, currently pursuing a bachelor's degreee in Computer Engineering at the University of Waterloo.</p>
 				<p className='intro-paragraph'>Let's start scrolling to learn more about me.</p>
+				<div className='button-container'>
+					<button onClick={handleScroll} className='scroll-button'>Start Scrolling</button>
+					<img src={ScrollIcon} alt='Scroll Down' className='scroll-icon' />
+				</div>
 			</div>
 		</section>
 	);
